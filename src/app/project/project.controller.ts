51f87330eed155af3981d436ef55774230dac827
Cândido Júnior <c.junyor88@gmail.com>
2021-05-16 04:45:12 -0300

    feat: project application agreement pdf

--- src/app/project/project.controller.ts
+++ src/app/project/project.controller.ts
@@ -15,7 +15,7 @@ import {
 import { ApiTags, ApiNotFoundResponse, ApiBadRequestResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { ProjectService } from './project.service';
 import { ProfessorService } from '../professor/professor.service';
-import { CreateProjectDto, UpdateProjectDto, ProjectResponseDto, CreateProjectApplicationDto } from './project.dto';
+import { CreateProjectDto, UpdateProjectDto, ProjectResponseDto, CreateProjectApplicationDto, ProjectAgreementPdfResponseDto } from './project.dto';
 import { FindAllParams, FindByIdParam } from '../professor/professor.dto';
 import { Application } from '.prisma/client';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -23,6 +23,8 @@ import { RequestWithUser } from '../auth/auth.interface';
 import { Roles } from '../../decorators/roles.decorator';
 import { Role } from '../../enums/role.enum';
 import { RolesGuard } from '../../guards/roles.guard';
+import { DataUploadService } from '../data.upload/data.upload.service';
+import { PdfService } from '../pdf/pdf.service';
 
 @ApiTags('Project')
 @Controller('project')
@@ -30,6 +32,8 @@ export class ProjectController {
   constructor(
     private readonly projectService: ProjectService,
     private readonly professorService: ProfessorService,
+    private readonly dataUploadServie: DataUploadService,
+    private readonly pdfService: PdfService,
   ) {}
 
   @Post()
@@ -70,6 +74,7 @@ export class ProjectController {
   @ApiBearerAuth()
   @ApiNotFoundResponse({ description: 'Project or Student not found.' })
   @ApiBadRequestResponse({ description: 'Student already have an application in progress for this project.' })
+  @ApiBadRequestResponse({ description: 'Student was already accepted in other project.' })
   async createProjectApplication(
     @Request() req: RequestWithUser,
     @Body() { projectId }: CreateProjectApplicationDto,
@@ -86,6 +91,18 @@ export class ProjectController {
     });
 
     if (applications.length) throw new BadRequestException('Student already have an application for this project.');
+
+    const acceptedApps = await this.projectService.applications({
+      where: {
+        studentId: req.user.student?.id,
+        status: 'ACCEPTED',
+        project: {
+          status: 'IN_PROGRESS',
+        },
+      },
+    });
+
+    if (acceptedApps.length) throw new BadRequestException('Student was already accepted in other project.');
 
     const application = await this.projectService.createProjectApplication(projectId, req.user.student?.id);
 
@@ -132,6 +149,31 @@ export class ProjectController {
     return this.projectService.projects({ skip, take, orderBy: { createdAt: 'desc' } });
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('application/:id/agreement')
+  @Roles(Role.Student)
+  @ApiBearerAuth()
+  @ApiNotFoundResponse({ description: 'Application not found.' })
+  @ApiBadRequestResponse({ description: 'Application was not accepted' })
+  async generateProjectApplicationAgreement(
+    @Request() req: RequestWithUser,
+    @Param() { id }: FindByIdParam,
+  ): Promise<ProjectAgreementPdfResponseDto> {
+    const application = await this.projectService.applicationWithProfessor({ id });
+
+    if (!application || application.studentId !== req.user.student?.id) throw new NotFoundException('Application not found.');
+
+    if (application.status !== 'ACCEPTED') throw new BadRequestException('Application was not accepted');
+
+    const { project: { professorAdvisor: { professor } } } = application;
+
+    const buffer =  await this.pdfService.createPdf(professor.name, professor.email, application.project.title, req.user.student.name);
+
+    const [fileUrl] = await this.dataUploadServie.sendFileUpload([{ filename: `TERMO_ACEITACAO_${professor.name}_${req.user.student.name}_${application.project.title}`, buffer }]);
+
+    return { fileUrl };
+  }
+
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
   @Roles(Role.ProfessorAdvisor)
@@ -165,7 +207,7 @@ export class ProjectController {
   ): Promise<ProjectResponseDto> {
     const project = await this.projectService.project({ id });
 
-    if (!project || project.deletedAt || project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
+    if (!project || project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
 
     if (project.status === 'DISABLED') throw new BadRequestException('Project is already deactivated.');
 
@@ -194,7 +236,7 @@ export class ProjectController {
 
     if (application.status !== 'IN_PROGRESS') throw new BadRequestException('Application was already acepted or rejected.');
 
-    if (!application || application.deletedAt || application.project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
+    if (!application || application.project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
 
     if (application.project.status === 'IN_PROGRESS') throw new BadRequestException('Project already have a student work on it.');
 
@@ -219,7 +261,7 @@ export class ProjectController {
 
     if (application.status !== 'IN_PROGRESS') throw new BadRequestException('Application was already acepted or rejected.');
 
-    if (!application || application.deletedAt || application.project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Application not found.');
+    if (!application || application.project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Application not found.');
 
     return this.projectService.rejectProjectApplication(id, req.user.professor?.professorAdvisor?.id);;
   }
@@ -236,7 +278,7 @@ export class ProjectController {
     const application = await this.projectService.application({ id });
 
     if (!application || (application.project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id && application.studentId !== req.user.student?.id)) {
-      throw new NotFoundException(application, 'Application not found.');
+      throw new NotFoundException('Application not found.');
     }
 
     return this.projectService.deleteProjectApplication(id, application.project.professorAdvisorId);
@@ -253,7 +295,7 @@ export class ProjectController {
   ): Promise<ProjectResponseDto> {
     const project = await this.projectService.project({ id });
 
-    if (!project || project.deletedAt ||  project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
+    if (!project ||  project.professorAdvisorId !== req.user.professor?.professorAdvisor?.id) throw new NotFoundException('Project not found.');
 
     if (project.status === 'IN_PROGRESS') throw new BadRequestException('Can\'t delete a project in progress.');
 
